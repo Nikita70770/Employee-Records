c0f59d92c637378778ad1bbe84a9f79bcfbbc241
Nikita70770 <filippovni1999@gmail.com>
2023-09-07 22:57:46 +0300

    Create a new main.js component and import it into the app.js component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import Header from '../header/header';
+import Main from '../main/main';
 
 const App = () => {
     return (
         <div className="app">
             <Header />
+            <Main></Main>
         </div>
     );
 };
---
+++ src/components/main/main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+    return (
+        <main className="main">
+            <div className="container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
