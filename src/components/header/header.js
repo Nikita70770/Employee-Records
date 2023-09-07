c2c90da50e7d357c953c3e24aebfd8a2b306d7c6
Nikita70770 <filippovni1999@gmail.com>
2023-09-07 22:07:05 +0300

    Create a header.css file and import it into header.js file

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,12 @@
+import './header.css';
+
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
                 <div className="header-inner">
                     <h1 className="header-title">Employee Records</h1>
+                    <h2 className="header-subtitle">Total number of employees: n</h2>
                 </div>
             </div>
         </header>
