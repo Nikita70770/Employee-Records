47f65f8a3dca3f0718761561c537b00ce821a4d5
Nikita70770 <filippovni1999@gmail.com>
2023-09-07 18:12:13 +0300

    Create a directory called components with the necessary subdirectories and files

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Lorem</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    );
+};
+
+export default App;
