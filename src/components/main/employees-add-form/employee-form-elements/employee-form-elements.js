c6d88b0d58a4023ebe70deb3cbb90e21318e03f7
Nikita70770 <filippovni1999@gmail.com>
2023-09-10 21:47:42 +0300

    Create employee-form-elements.css and employees-add-form.css files. Import the created files with styles into the employee-form-elements.js and employees-add-form.js files.

--- src/components/main/employees-add-form/employee-form-elements/employee-form-elements.js
+++ src/components/main/employees-add-form/employee-form-elements/employee-form-elements.js
@@ -1,14 +1,14 @@
+/* import styles */
+import './employee-form-elements.css';
+
 const EmployeeFormElements = () => {
     return (
-        <div className="empl-form-elems">
-            <h2 className="empl-form-elems-title">Add a new employee</h2>
-            <div className="empl-form-elems-inner">
-                <input className="inpt inpt-empl-name" type="text" placeholder="What's his name?" />
-                <input className="inpt inpt-empl-name" type="text" placeholder="Salary?" />
-                <button className="btn-add-empl" type="submit">
-                    Добавить
-                </button>
-            </div>
+        <div className="empl-form-elements">
+            <input className="inpt inpt-empl-name" type="text" placeholder="What's his name?" />
+            <input className="inpt inpt-empl-salary" type="text" placeholder="Salary?" />
+            <button className="btn-add-empl" type="submit">
+                Add employee
+            </button>
         </div>
     );
 };
--- src/components/main/employees-add-form/employees-add-form.js
+++ src/components/main/employees-add-form/employees-add-form.js
@@ -1,10 +1,17 @@
+/* import components */
 import EmployeeFormElements from './employee-form-elements/employee-form-elements';
+
+/* import styles */
+import './employees-add-form.css';
 
 const EmployeeAddForm = () => {
     return (
         <section className="empl-add-form">
             <div className="container">
-                <EmployeeFormElements />
+                <div className="empl-add-form-inner">
+                    <h2 className="empl-form-title">Add a new employee</h2>
+                    <EmployeeFormElements />
+                </div>
             </div>
         </section>
     );
