939849fe33535df109eacb9634047b7fcd73e127
Nikita70770 <filippovni1999@gmail.com>
2023-10-09 13:00:08 +0300

    Remake functional components Main, EmployeesList and EmployeeItem into class components

--- src/components/main/employees-list/employees-list.js
+++ src/components/main/employees-list/employees-list.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 /* import components */
 import EmployeeItemTitle from './employees-list-item/employees-item-title';
 import EmployeeItem from './employees-list-item/employee-item';
@@ -5,23 +7,29 @@ import EmployeeItem from './employees-list-item/employee-item';
 /* import styles */
 import './employees-list.css';
 
-const EmployeesList = ({ data }) => {
-    const elements = data.map((item, index) => <EmployeeItem key={index.toString()} employee={item} />);
+class EmployeesList extends Component {
+    constructor(props) {
+        super(props);
+        this.dataEmpl = this.props.data;
+    }
 
-    return (
-        <section className="employees-list">
-            <div className="container">
-                <div className="employees-list-inner">
-                    <table className="employees-table" cellSpacing="0" cellPadding="0">
-                        <tbody>
-                            <EmployeeItemTitle />
-                            {elements}
-                        </tbody>
-                    </table>
+    render() {
+        const elements = this.dataEmpl.map((item, index) => <EmployeeItem key={index.toString()} employee={item} />);
+        return (
+            <section className="employees-list">
+                <div className="container">
+                    <div className="employees-list-inner">
+                        <table className="employees-table" cellSpacing="0" cellPadding="0">
+                            <tbody>
+                                <EmployeeItemTitle />
+                                {elements}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    }
+}
 
 export default EmployeesList;
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,23 +1,31 @@
+import { Component } from 'react';
+
+/* import Components */
 import SearchEmployee from './search-employee/search-employee';
 import EmployeeAddForm from './employees-add-form/employees-add-form';
 import EmployeesList from './employees-list/employees-list';
 
-const Main = () => {
-    const data = [
-        { name: 'John C.', post: 'Programmer', salary: 2500, increase: false },
-        { name: 'Kate W', post: 'Designer', salary: 1250, increase: true },
-        { name: 'Bob D.', post: 'Director', salary: 8500, increase: false },
-        { name: 'Tom C.', post: 'Manager', salary: 1190, increase: true },
-        { name: 'Cindy S.', post: 'Secretary', salary: 1050, increase: true }
-    ];
+class Main extends Component {
+    constructor() {
+        super();
+        this.data = [
+            { name: 'John C.', post: 'Programmer', salary: 2500, increase: false },
+            { name: 'Kate W', post: 'Designer', salary: 1250, increase: true },
+            { name: 'Bob D.', post: 'Director', salary: 8500, increase: false },
+            { name: 'Tom C.', post: 'Manager', salary: 1190, increase: true },
+            { name: 'Cindy S.', post: 'Secretary', salary: 1050, increase: true }
+        ];
+    }
 
-    return (
-        <main className="main">
-            <SearchEmployee />
-            <EmployeeAddForm />
-            <EmployeesList data={data} />
-        </main>
-    );
-};
+    render() {
+        return (
+            <main className="main">
+                <SearchEmployee />
+                <EmployeeAddForm />
+                <EmployeesList data={this.data} />
+            </main>
+        );
+    }
+}
 
 export default Main;
